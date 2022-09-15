5f9a6e82bb83883c79c16fb5b8e73444241696fd
shahadot99999 <shahadot99999@gmail.com>
2022-09-15 19:15:39 +0600

    time inter show second

--- script.js
+++ script.js
@@ -146,8 +146,6 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = parseInt((currentTime - startTime) / 1000);
-
-
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
 
